7f6999b56046d97838731775a9a8570942ee77e8
Peter Fortuna <peter.j.fortuna@gmail.com>
2025-12-13 09:03:20 -0500

    Add description for language game

--- pages/language-game.js
+++ pages/language-game.js
@@ -3,10 +3,10 @@ pages['language-game'] = `
 <div class="container mt-5">
     <button class="btn btn-outline-primary mb-4" onclick="goBack()">← Back</button>
     <h1 class="header">Language Learning Game</h1>
-    <h1>Product</h1>
-    <p>Your content here...</p>
+    <h1>Technical Overview</h1>
+    <p>This project was build in React Native (Expo) and Typescript. It was build over the course of a few weeks</p>
 
-    <h1>Implementation</h1>
-    <p>Your content here...</p>
+    <h1>Product Overview</h1>
+    <p>The app is a simple language learning game that was designed from the ground up with generative AI image and text generation in mind. Tens of thousands of sentence and image pairs were systematically created using approximately 500 vocab words. The app is designed to create a more intuitive user experience by accompanying each sentence with an image that clearly demonstrates the meaning of the sentence.</p>
 </div>
 `;
