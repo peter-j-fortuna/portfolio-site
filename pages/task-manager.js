435f17caebfd56d09649a7b6b4c22b7ac57d456e
Peter Fortuna <peter.j.fortuna@gmail.com>
2025-12-15 12:54:00 -0500

    Remove Resume Rag

--- pages/task-manager.js
+++ pages/task-manager.js
@@ -3,7 +3,7 @@ pages['task-manager'] = `
     <button class="btn btn-outline-primary mb-4" onclick="goBack()">← Back</button>
     <h1 class="header">Task Manager</h1>
     <h1>Overview</h1>
-     <div class="row align-items-stretch">
+    <div class="row align-items-stretch">
         <div class="col-4 d-flex align-items-center">
             <p>This application is a CRUD task manager built with Rails following a Udemy course. It implements login/logout and to-do list management using the standard MVC Rails architecture. I added a daily/weekly reminder feature that sends scheduled emails as well, shown to the right. This project was primarily done as a way to familiarize myself with software development using Ruby on Rails, which has been my primary responsibility at Corporate Traditions.</p>
         </div>
@@ -11,6 +11,5 @@ pages['task-manager'] = `
             <img src="assets/email-reminder.png" alt="Email Reminder" class="img-fluid">
         </div>
     </div>
-
 </div>
 `;
