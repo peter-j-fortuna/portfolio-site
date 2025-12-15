b54268944d33d7977872029dea8fa3489fd314a5
Peter Fortuna <peter.j.fortuna@gmail.com>
2025-12-15 12:31:08 -0500

    Update text on task manager section

--- pages/task-manager.js
+++ pages/task-manager.js
@@ -5,7 +5,7 @@ pages['task-manager'] = `
     <h1>Overview</h1>
      <div class="row align-items-stretch">
         <div class="col-4 d-flex align-items-center">
-            <p>This application is a CRUD task manager built with Rails following a Udemy course. It implements login/logout, scheduled emails, and task management using the standard MVC Rails architecture. I added a daily/weekly reminder feature that sends scheduled emails as well, shown to the right.</p>
+            <p>This application is a CRUD task manager built with Rails following a Udemy course. It implements login/logout and to-do list management using the standard MVC Rails architecture. I added a daily/weekly reminder feature that sends scheduled emails as well, shown to the right. This project was primarily done as a way to familiarize myself with software development using Ruby on Rails, which has been my primary responsibility at Corporate Traditions.</p>
         </div>
         <div class="col-8">
             <img src="assets/email-reminder.png" alt="Email Reminder" class="img-fluid">
