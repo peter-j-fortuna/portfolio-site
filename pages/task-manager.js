aa6041d130952bfd9d5443389bc9648ec307993c
Peter Fortuna <peter.j.fortuna@gmail.com>
2025-12-15 09:08:46 -0500

    Task manager description added

--- pages/task-manager.js
+++ pages/task-manager.js
@@ -3,7 +3,14 @@ pages['task-manager'] = `
     <button class="btn btn-outline-primary mb-4" onclick="goBack()">← Back</button>
     <h1 class="header">Task Manager</h1>
     <h1>Overview</h1>
-    <p>Your content here...</p>
-    <p>https://github.com/peter-j-fortuna/task-manager</p>
+     <div class="row align-items-stretch">
+        <div class="col-4 d-flex align-items-center">
+            <p>This application is a CRUD task manager built with Rails following a Udemy course. It implements login/logout, scheduled emails, and task management using the standard MVC Rails architecture. I added a daily/weekly reminder feature that sends scheduled emails as well, shown to the right.</p>
+        </div>
+        <div class="col-8">
+            <img src="assets/email-reminder.png" alt="Email Reminder" class="img-fluid">
+        </div>
+    </div>
+
 </div>
 `;
